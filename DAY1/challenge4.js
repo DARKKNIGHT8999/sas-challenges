bd3f53715de29124b035c50678c3433cacc6e1fe
DARKKNIGHT8999 <nightdragonice89@gmail.com>
2026-09-17 12:43:14 +0200

    work

---
+++ DAY1/challenge4.js
@@ -0,0 +1,6 @@
+let distance=500;
+let carburant=35;
+let consommation=(carburant/distance)*100;
+console.log("distance: " + distance + "KM");
+console.log("carburant: " + carburant + "litres");
+console.log("consommation: " + consommation + "L/100KM");
