a58ba45d92e1928d02aab369d5b27e6fba817635
DARKKNIGHT8999 <nightdragonice89@gmail.com>
2026-09-18 12:58:10 +0200

    work

---
+++ DAY2/challenge3.js
@@ -0,0 +1,24 @@
+const prompt = require('prompt-sync')();
+console.log("menu");
+console.log("1.afficher le profil");
+console.log("2.afficher les paramètres");
+console.log("3.afficher les notifications");
+console.log("4.se déconnecter");
+let choix = Number(prompt("choix: "));
+switch(choix){
+case 1:
+    console.log("afficher le profil");
+    break;    
+case 2:
+    console.log("afficher les paramétres");
+    break;
+case 3:
+    console.log("afficher les notifications");
+    break;
+case 4:
+    console.log("se déconnecter");
+    break;
+default:
+    console.log("choix invalide.");
+    break;          
+}
