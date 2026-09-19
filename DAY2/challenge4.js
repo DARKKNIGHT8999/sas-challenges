8f1c8738173662e2bde453a4d643a2b8f74ae112
DARKKNIGHT8999 <nightdragonice89@gmail.com>
2026-09-19 10:31:32 +0200

    work

--- DAY2/challenge4.js
+++ DAY2/challenge4.js
@@ -1,5 +1,5 @@
 const prompt = require('prompt-sync')();
 let nombre = Number(prompt("nombre de participant: "));
-for (i=1;i<=nombre;i++);{
+for (let i=1;i<=nombre;i++){
 console.log("participant" + i);
 }
---
+++ DAY3/challenge1.js
@@ -0,0 +1,4 @@
+let tab = [ 100 , 200 , 300 , 400 , 500 ];
+for(i=0;i<tab.length;i++){
+    console.log(tab[i]);
+}
